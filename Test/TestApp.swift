ecc6e53fc669c150240a6f3782325ffa1bb81ad1
jatin-iOS <jatin.gupta@eazydiner.com>
2025-01-08 18:38:30 +0530

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jatin Gupta on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
